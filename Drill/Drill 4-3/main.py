50547500842f481bb956fee22a364fce40513ba9
kimduckbae <qlccksdlf@gmail.com>
2019-10-09 14:30:58 +0900

    add MousePointer

---
+++ Drill/Drill 4-3/main.py
@@ -0,0 +1,57 @@
+import pico2d as p
+from enum import Enum
+MONITORX = 1280
+MONITORY = 1024
+p.open_canvas(MONITORX,MONITORY)
+frame = 0
+cx = 600
+cy = 200
+mx = 0
+my = 0
+targetX = 0
+targetY = 0
+
+class Direct(Enum):
+    e_right = 1,
+    e_left = -1,
+class State(Enum):
+    e_idle = 0,
+    e_running = 1
+def handle_event():
+    global mx
+    global my
+    global targetX,targetY
+    event = p.get_events()
+    for e in event:
+        if e == p.SDL_MOUSEBUTTONDOWN:
+            targetX = e.x
+            targetY = e.y
+        if e.type == p.SDL_MOUSEMOTION:
+            mx = e.x
+            my = MONITORY - 1 - e.y
+
+grass = p.load_image('KPU_GROUND.png')
+char = p.load_image('animation_sheet.png')
+mouse = p.load_image('hand_arrow.png')
+direct = Direct.e_right
+state = State.e_idle
+p.hide_cursor()
+while(True):
+    p.clear_canvas()
+    grass.draw(MONITORX / 2, MONITORY / 2)
+    if direct == Direct.e_right:
+        if state == State.e_idle:
+            char.clip_draw(frame * 100, 300, 100, 100, cx, cy)
+        else:
+            char.clip_draw(frame * 100, 100, 100, 100, cx, cy)
+    else:
+        if state == State.e_idle:
+            char.clip_draw(frame * 100, 200, 100, 100, cx, cy)
+        else:
+            char.clip_draw(frame * 100, 0, 100, 100, cx, cy)
+    handle_event()
+    mouse.draw_now(mx + 25,my -26)
+    p.update_canvas()
+    frame = (frame+1) % 8
+
+p.close_canvas()
